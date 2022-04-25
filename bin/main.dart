9375fa9912c90cdd1aed95ed7d65774228fd1bf8
AsliddinBoriyev <asliddinboriyev824@gmail.com>
2022-04-25 12:00:45 +0500

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:contact_app/models/contact_model.dart';
+import 'package:contact_app/services/file_service.dart';
+import 'package:contact_app/services/io_service.dart';
+
+void main() async {
+  FileService fileService = FileService();
+  await fileService.init();
+
+  String title = "";
+  String content = "";
+  String exit = "";
+
+  write("Iltimos note ni nomini kiriting: ");
+  title = read();
+  Note note = await fileService.readFile(title);
+  print(note);
+
+  // String path = await fileService.createFile(title);
+  // writeln("Note ni yozishingiz mumkin(Yozib tugatganingizda 'Save' so'zini yozsangiz note saqlab qo'yiladi)");
+  // while(exit != "Save") {
+  //   exit = read();
+  //   if(exit == "Save") {
+  //     break;
+  //   }
+  //   content += (exit + "\n");
+  // }
+  //
+  // Note note = Note(title: title, content: content);
+  //
+  // await fileService.writeFile(note, path).then((value) => print("Note muvofaqiyatli saqlandi"));
+}
